4c6d9e2eb37f58a6704e7f2556a13655c19d87da
Sheldon Callahan <callsheldon@gmail.com>
2015-12-15 19:42:03 +0000

    Added Lodash Loop for more consise code added server.js to run project from node

---
+++ app/js/app.js
@@ -0,0 +1,79 @@
+// GET PARTNERS FROM API  
+function GET_partners($http, $q) {
+    var self = this;
+    self.getPartnersP = $q.defer();
+
+    self.getPartners = function() {
+        // API REQUEST   
+        $http.get('https://api.kivaws.org/v1/partners.json').
+        success(function(data, status, headers, config) {
+            self.getPartnersP.resolve(data.partners);
+        }).
+        error(function(data, status, headers, config) {});
+    };
+    return self;
+}
+//Partner Page Controller
+function PartnersCtrl(GET_partners, $scope) {
+    var self = this;
+    self.data;
+    self.countryList = [];
+    // CHECK IF COUNTRY HAS FAVIORTE IN LOCAL STORAGE AND SET IF FAVORITE EXISITS 
+    if (amplify.store("country")) {
+        self.selectedCountry = amplify.store("country").country;
+        self.favoriteCountry = amplify.store("country").country;
+    }
+    else {
+        self.selectedCountry;
+        self.favoriteCountry;
+    }
+
+    // SET FAVORITE AND SAVE TO LOCAL STORAGE
+    $scope.fav = function(value) {
+        self.favoriteCountry = value.country
+        self.selectedCountry = value.country;
+        amplify.store("country", {
+            'country': value.country
+        });
+    }
+
+    // SELECT COUNTRY WITH BUTTON
+    $scope.selectCountry = function(value) {
+        self.selectedCountry = value.country;
+    }
+
+    //PROMISE TO RUN WHEN DATA IS RETURNED FROM THE API
+    GET_partners.getPartners();
+    GET_partners.getPartnersP.promise.then(function(data) {
+        self.data = data;
+
+        //LOOP THROUGH AVAIBLE COUNTRIES AND REMOVE DUPS
+        function getCountries() {
+            var countries = []
+            
+            //LODASH LOOP THROUGH DATA TO FIND COUNTRIES 
+            _.each(self.data, function(item, i){
+                _.each(self.data[i].countries, function(country, c){
+                    //PUSH COUNTRIES TO ARRAY
+                    countries.push(self.data[i].countries[c].name);
+                })
+            })
+            
+            return countries
+        }
+        // ONLY SHOW UNIQUE NAMES
+        self.countryList = _.uniq(getCountries());
+    })
+}
+
+
+// INI ANGULAR APP SERVICE AND CONTROLLER
+angular.module('app', [])
+    .service('GET_partners', ['$http', '$q', GET_partners])
+    .controller('PartnersCtrl', ['GET_partners', '$scope', PartnersCtrl]);
+
+
+// INI FOUNDATION
+$(document).ready(function() {
+    $(document).foundation();
+});
